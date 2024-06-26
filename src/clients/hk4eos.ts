f259f6d6e7336480c4fee4778cb06aaf6fc0f389
1146839821 <1146839821@qq.com>
2024-06-26 11:56:53 +0800

    wine7.7-gptk

--- src/clients/hk4eos.ts
+++ src/clients/hk4eos.ts
@@ -20,8 +20,8 @@ import {
 } from "./secret";
 
 export const DEFAULT_WINE_DISTRO_URL =
-  "https://github.com/3Shain/wine/releases/download/v9.2-mingw/wine.tar.gz";
-export const DEFAULT_WINE_DISTRO_TAG = "v9.2-mingw";
+"https://github.com/1146839821/wine/releases/download/HSR/wine.tar.gz";
+export const DEFAULT_WINE_DISTRO_TAG = "wine7.7-gptk";
 
 const SERVER_DEFINITION: Server = {
   id: "hk4e_global",
